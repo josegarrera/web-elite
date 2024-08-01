a4c9811bb8b3144d4ad5faa7ec8f35dffa20efdf
Jose Garrera <josegarrera@gmail.com>
2024-08-01 09:48:21 -0300

    chore: Refactor code to improve readability and maintainability

--- clase02/src/index.ts
+++ clase02/src/index.ts
@@ -1,47 +1,13 @@
-interface Character {
-  id: number;
-  name: string;
-  status: string;
-  species: string;
-  type: string;
-  gender: string;
-  origin: Location;
-  location: Location;
-  image: string;
-  episode: string[];
-  url: string;
-  created: Date;
-}
-
-interface Location {
-  name: string;
-  url: string;
-}
+import { Character } from './interfaces';
 
 document.addEventListener('DOMContentLoaded', async () => {
   const container = document.getElementById('container');
 
   try {
-    const response = await fetch(
-      'https://rickandmortyapi.com/api/character'
-    );
-    const data = await response.json();
-    const firstNineCharacters: Character[] = data.results.slice(0, 9);
-    firstNineCharacters.forEach(character => {
-      const card = document.createElement('div');
-      card.classList.add('card');
-
-      const img = document.createElement('img');
-      img.src = character.image;
-      img.alt = `${character.name} Image`;
-
-      const name = document.createElement('h3');
-      name.textContent = character.name;
-
-      card.appendChild(img);
-      card.appendChild(name);
-      container.appendChild(card);
-    });
+    const allCharacters: Character[] = await fetchCharactersData();
+    const firstNineCharacters: Character[] =
+      extractFirstNineResults(allCharacters);
+    renderCharacterCards(firstNineCharacters, container);
   } catch (error) {
     console.error(error);
     const errorMessage = document.createElement('h1');
@@ -50,3 +16,59 @@ document.addEventListener('DOMContentLoaded', async () => {
     container.appendChild(errorMessage);
   }
 });
+
+const fetchCharactersData = async (): Promise<Character[]> => {
+  const response = await fetch(
+    'https://rickandmortyapi.com/api/character'
+  );
+  const data = await response.json();
+  return data.results;
+};
+
+const extractFirstNineResults = (
+  results: Character[]
+): Character[] => {
+  return results.slice(0, 9);
+};
+
+const renderCharacterCards = (
+  firstNineCharacters: Character[],
+  container: HTMLElement
+) => {
+  firstNineCharacters.forEach(character => {
+    const card = createCharacterCard(character);
+    container.appendChild(card);
+  });
+};
+
+const createCharacterCard = character => {
+  const card = buildCardElement();
+
+  const img = buildImageElement(character);
+
+  const name = buildTitleElement(character);
+
+  card.appendChild(img);
+  card.appendChild(name);
+
+  return card;
+};
+
+const buildCardElement = () => {
+  const card = document.createElement('div');
+  card.classList.add('card');
+  return card;
+};
+
+const buildImageElement = (character: any) => {
+  const img = document.createElement('img');
+  img.src = character.image;
+  img.alt = `${character.name} Image`;
+  return img;
+};
+
+const buildTitleElement = (character: any) => {
+  const name = document.createElement('h3');
+  name.textContent = character.name;
+  return name;
+};
